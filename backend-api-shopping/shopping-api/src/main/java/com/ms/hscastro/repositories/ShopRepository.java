cac5d9fe9e3ebd5ff88b02d4e7c8d957a4700b60
Halyson Santos <halisonsc5@gmail.com>
2021-10-10 18:01:53 -0300

    Implemention of ShopRepository in shopping-api

--- backend-api-shopping/shopping-api/src/main/java/com/ms/hscastro/repositories/ShopRepository.java
+++ backend-api-shopping/shopping-api/src/main/java/com/ms/hscastro/repositories/ShopRepository.java
@@ -1,11 +1,9 @@
 package com.ms.hscastro.repositories;
 
+import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ms.hscastro.entities.Shop;
@@ -14,14 +12,9 @@ import com.ms.hscastro.entities.Shop;
 @Repository
 public interface ShopRepository extends JpaRepository<Shop, Long> {
 	
-	Optional<Shop> findByIdentifier(String identified);
+	List<Shop> findAllByUsersIdentifier(String userIdentified);
 	
-	@Query(value = "select p.nome, p.preco, "
-			+ "p.identifier, p.descricao, "
-			+ "from product p"
-			+ "join category c on p.category.id = c.id"
-			+ "where c.id =:categoryId ")
-	List<Shop> getShopByCategoryId(@Param("categoryId") Long categoryId);
+	List<Shop> findAllByTotalGreaterThan(float total);
 	
-	Shop findByShopIdentifier(String identified);
+	List<Shop> findAllBydateGreaterThan(Date date);
 }
