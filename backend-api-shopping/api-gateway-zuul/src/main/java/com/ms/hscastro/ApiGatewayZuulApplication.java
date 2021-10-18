4c3cc5eee9e16ea613e4196eebe3da00e415b01d
Halyson Santos <halisonsc5@gmail.com>
2021-10-17 21:33:22 -0300

    Add gateway api

---
+++ backend-api-shopping/api-gateway-zuul/src/main/java/com/ms/hscastro/ApiGatewayZuulApplication.java
@@ -0,0 +1,15 @@
+package com.ms.hscastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayZuulApplication.class, args);
+	}
+
+}
